9a8aa795dbf76c519727527eeeab6921673b5b5e
Winston Chang <winston@posit.co>
2025-08-25 13:01:50 -0500

    Update utils files

--- examples/2-inputs/py/utils.py
+++ examples/2-inputs/py/utils.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
 from shiny import ui
 from shiny.html_dependencies import shiny_deps
+from shiny.types import Jsonifiable
+from shiny.render.renderer import Renderer
 
 
 def page_bare(*args: ui.TagChild, title: str | None = None, lang: str = "en") -> ui.Tag:
@@ -8,3 +12,28 @@ def page_bare(*args: ui.TagChild, title: str | None = None, lang: str = "en") ->
         ui.tags.body(shiny_deps(False), *args),
         lang=lang,
     )
+
+
+class render_object(Renderer[Jsonifiable]):
+    """
+    Reactively render arbitrary JSON object.
+
+    This is a generic renderer that can be used to render any Jsonifiable data.
+    It sends the data to the client-side and let the client-side code handle the
+    rendering.
+
+    Returns
+    -------
+    :
+        A decorator for a function that returns a Jsonifiable object.
+
+    """
+
+    def __init__(
+        self,
+        _fn: Optional[ValueFn[str]] = None,
+    ) -> None:
+        super().__init__(_fn)
+
+    async def transform(self, value: str) -> Jsonifiable:
+        return value
--- examples/2-inputs/r/utils.R
+++ examples/2-inputs/r/utils.R
@@ -9,3 +9,29 @@ barePage <- function(..., title = NULL, lang = NULL) {
   attr(ui, "lang") <- lang
   ui
 }
+
+#' Reactively render arbitrary JSON object data.
+#'
+#' This is a generic renderer that can be used to render any Jsonifiable data.
+#' It sends the data to the client-side and let the client-side code handle the
+#' rendering.
+renderObject <- function(
+  expr,
+  env = parent.frame(),
+  quoted = FALSE,
+  outputArgs = list(),
+  sep = " "
+) {
+  func <- installExprFunction(expr, "func", env, quoted, label = "renderObject")
+
+  createRenderFunction(
+    func,
+    function(value, session, name, ...) {
+      value
+    },
+    function(...) {
+      stop("Not implemented")
+    },
+    outputArgs
+  )
+}
